b76da92d7a2112dc63bbb007a19d8fcfd1aa91ef
turi0002 <trui0002@gmail.com>
2017-11-24 11:30:17 +0200

    shoter

--- app/src/main/java/com/example/user/ex7/MainActivity.java
+++ app/src/main/java/com/example/user/ex7/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.user.ex7;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    public final int registerCode=1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void onToggleClicked(View v) {
+        Intent myIntent = new Intent(MainActivity.this, RegisterActivity.class);
+        MainActivity.this.startActivityForResult(myIntent,registerCode);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode,data);
+        if(requestCode ==registerCode && resultCode == RESULT_OK ){
+            String lName = data.getStringExtra("lName");
+            String fName = data.getStringExtra("fName");
+            String gender = data.getStringExtra("gender");
+            RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.rl);
+            TextView intro = (TextView)findViewById(R.id.introd) ;
+            Bundle dict = data.getExtras();
+            if(dict.get("gender").equals("male"))
+            {
+
+            }
+            else{
+                intro.setText("welcome back Miss." +fName+", "+lName);
+            }
+            Button button = (Button)findViewById(R.id.reg) ;
+            button.setText("again...");
+        }
     }
 }
--- app/src/main/java/com/example/user/ex7/RegisterActivity.java
+++ app/src/main/java/com/example/user/ex7/RegisterActivity.java
@@ -1,13 +1,38 @@
 package com.example.user.ex7;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends Activity {
-
+    public final int Register_Code =1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        }
+        public void returnToMain(View view)
+        {
+            Intent intent = new Intent(this,MainActivity.class);
+            EditText lName = (EditText)findViewById(R.id.efName);
+            EditText fName = (EditText)findViewById(R.id.elName);
+            String lastName = lName.getText().toString();
+            String firstName = lName.getText().toString();
+            RadioGroup radioGroup = (RadioGroup)findViewById(R.id.rGroup);
+            int genderId = radioGroup.getCheckedRadioButtonId();
+            RadioButton genderB = (RadioButton) findViewById(genderId);
+            String gender = genderB.getText().toString();
+            intent.putExtra("lName",lastName);
+            intent.putExtra("fName",firstName);
+            intent.putExtra("gender",gender);
+            setResult(RESULT_OK,intent);
+            finish();
+        }
     }
-}
+
